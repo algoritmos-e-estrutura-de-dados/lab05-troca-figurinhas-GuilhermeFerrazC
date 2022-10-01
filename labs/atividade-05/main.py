ab9fd1c06077b945f98964a505bfcc8aa7a3561b
Guilherme Ferraz Candido <107952810+GuilhermeFerrazC@users.noreply.github.com>
2022-09-30 22:31:30 -0300

    Add files via upload

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,12 +1,25 @@
-def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+def maximizar_troca_de_figurinhas(figurinhasDaMaria, figurinhasDoJoao):
+    trocas = 0
+   
+   #verifica quem possue uma quantidade maior de figurinhas
+    menorFigurinha = figurinhasDaMaria if len(figurinhasDaMaria) <= len(figurinhasDoJoao) else figurinhasDoJoao
+    maiorFigurinha = figurinhasDaMaria if len(figurinhasDaMaria) >= len(figurinhasDoJoao) else figurinhasDoJoao
+   
+    for index, n_Figurinha in enumerate(menorFigurinha):
+        if not n_Figurinha in maiorFigurinha:
+            for n in range(index, len(maiorFigurinha)):
+                if n_Figurinha != maiorFigurinha[n]:
+                    aux = maiorFigurinha[n]
+                    maiorFigurinha[n] = n_Figurinha
+                    menorFigurinha[index] = aux
+                    trocas = trocas + 1
+                    break
+    return trocas
 
 
 
 if __name__ == '__main__':
     A, B = input().split(' ')
-    figurinhas_da_maria = input().split(' ')
-    figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
+    figurinhasDaMaria = input().split(' ')
+    figurinhasDoJoao = input().split(' ')
+    maximizar_troca_de_figurinhas(figurinhasDaMaria, figurinhasDoJoao)
